42c9ee7447bcc94d0821fc60c4de5d948fb6651a
Serhii Mamchur <kovaniysvit@gmail.com>
2025-02-24 15:32:58 +0100

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -355,6 +355,19 @@ const people = [
 ];
 
 // eslint-disable-next-line no-console
-console.log(people); // you can remove it
 
-// write your code here
+const arrOfPeople = document.querySelector('.dashboard');
+
+people.forEach((person) => {
+  const row = `<tr>
+                <th>${person.name}</th>
+                <th>${person.sex}</th>
+                <th>${person.born}</th>
+                <th>${person.died}</th>
+                <th>${person.died - person.born}</th>
+                <th>${Math.ceil(person.died / 100)}</th>
+               </tr>`;
+
+  arrOfPeople.innerHTML += row;
+});
+// console.log(arrOfPeople)
